4eb2a302334342feebfd7cfd4e0ade81ea1bdaea
salma-anika <anikaavaa@gmail.com>
2023-01-20 01:25:20 +0600

    contact form create

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div className='contact-form'>
+      <div className='w-left'>
+        <div className='awesome'>
+            <span>Get in Touch</span> 
+            <span>Contact Me</span> 
+            <div className='blur s-blur absolute h-[11rem] w-[21rem] top-[13rem] left-[-18rem] ' style={{background:'#ABF1FF94'}}></div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default contact
