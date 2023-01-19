f5d1d95730047a8987c8da57d11ab04bc464f9de
salma-anika <anikaavaa@gmail.com>
2023-01-20 01:35:45 +0600

    form create

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -10,7 +10,15 @@ const contact = () => {
             <div className='blur s-blur absolute h-[11rem] w-[21rem] top-[13rem] left-[-18rem] ' style={{background:'#ABF1FF94'}}></div>
         </div>
       </div>
-      
+      <div className='c-right'>
+        <form>
+            <input type="text" name="user_name" className="user" placeholder="Enter Name"/>
+            <input type="email" name="email" className="user" placeholder="Enter Email"/>
+            <textarea type="text" name="message" className="user" placeholder="Message"/>
+            <input type="submit" value="send" className="button"/> 
+            <div className='blur t-blur1 top-[16rem] left-[38rem] absolute h-[11rem] w-[10rem]' style={{background:'var(--purple)'}}></div>
+        </form>
+      </div>
     </div>
   )
 }
