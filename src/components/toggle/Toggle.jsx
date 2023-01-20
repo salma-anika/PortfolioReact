1df55be27ddcaa489fdbef18f6b51cb1ff084b61
salma-anika <anikaavaa@gmail.com>
2023-01-20 22:12:44 +0600

    add css toggle and set toggle in navbar

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -3,11 +3,11 @@ import Sun from '@iconscout/react-unicons/icons/uil-sun';
 import Moon from '@iconscout/react-unicons/icons/uil-moon';
 const Toggle = () => {
   return (
-    <div className='toggle'>
+    <div className='toggle flex justify-between relative rounded-[1rem] cursor-pointer p-[2px]' style={{border:'3px solid var(--orange)'}}>
       <Sun/>
       <Moon/>
       <div className='t-button'>
-        
+
       </div>
     </div>
   )
