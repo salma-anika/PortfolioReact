93944bd965c174a3676e2163219886cdd431eaa7
salma-anika <anikaavaa@gmail.com>
2023-01-20 17:46:18 +0600

    footer social

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Github from '../../img/github.png';
+import LinkedIn from '../../img/linkedin.png';
+import Instagram from '../../img/instagram.png';
+const footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default footer
