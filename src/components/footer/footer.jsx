617d8d33c5405a1c53d4125c367af2794398c330
salma-anika <anikaavaa@gmail.com>
2023-01-20 21:42:09 +0600

    footer social-icons and feature

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import Github from '../../img/github.png';
-import LinkedIn from '../../img/linkedin.png';
-import Instagram from '../../img/instagram.png';
+import Wave from '../../img/wave.png';
+import Insta from '@iconscout/react-unicons/icons/uil-instagram'
+import Git from '@iconscout/react-unicons/icons/uil-github'
+import Facebook from '@iconscout/react-unicons/icons/uil-facebook'
 const footer = () => {
   return (
-    <div>
-      
+    <div className='footer'>
+      <img src={Wave} alt="" style={{width:'100%'}}/>
+
+      <div className='f-content'>
+<span>anikaavaa@gmail.com</span>
+<div className='f-icons'>
+<Insta/>
+<Git/>
+<Facebook/>
+</div>
+      </div>
     </div>
   )
 }
