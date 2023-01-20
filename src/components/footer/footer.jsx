3685d3ed301c1458932a6d414fb35216116034fc
salma-anika <anikaavaa@gmail.com>
2023-01-20 21:48:44 +0600

    add style in footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,10 +5,10 @@ import Git from '@iconscout/react-unicons/icons/uil-github'
 import Facebook from '@iconscout/react-unicons/icons/uil-facebook'
 const footer = () => {
   return (
-    <div className='footer'>
+    <div className='footer flex items-center relative mt-[-7rem]' style={{margin:'-0.5rem  -3.5rem'}}>
       <img src={Wave} alt="" style={{width:'100%'}}/>
 
-      <div className='f-content'>
+      <div className='f-content absolute flex flex-col justify-center items-center w-[100%] mt-16 gap-16 text-white'>
 <span>anikaavaa@gmail.com</span>
 <div className='f-icons'>
 <Insta/>
